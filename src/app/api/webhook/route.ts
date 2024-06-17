969747b3153e17d0cb29ea73c1b9048b2207d83e
Sebastian <sebastian.mazur.p@gmail.com>
2024-06-17 17:32:44 +0200

    route webhook

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,23 +1,21 @@
 import { revalidatePath } from "next/cache";
-import { NextRequest, NextResponse } from "next/server";
+import { type NextRequest } from "next/server";
 
-export async function POST(request: NextRequest): Promise<NextResponse> {
-  try {
-    // Przetwarzanie danych webhooka
-    const payload = await request.json();
+export async function POST(request: NextRequest): Promise<Response> {
+	const body: unknown = await request.json();
 
-    // Debugging: Możesz zalogować payload, aby zobaczyć jego strukturę
-    console.log("Received payload:", payload);
-
-    // Tutaj możesz dodać logikę do odświeżania ścieżek lub danych
-    // Przykładowo, jeśli chcesz odświeżyć stronę produktu:
-    await revalidatePath(`/products/${payload.data.id}`);
-
-    // Możesz dodać więcej logiki odświeżania w zależności od struktury payload i potrzeb
-
-    return NextResponse.json({ message: "Webhook received and revalidated" });
-  } catch (error) {
-    console.error("Error processing webhook:", error);
-    return NextResponse.json({ message: "Error processing webhook" }, { status: 500 });
-  }
+	if (
+		typeof body === "object" &&
+		body &&
+		"productId" in body &&
+		typeof body.productId === "string"
+	) {
+		console.log(`Revalidating /product/${body.productId}`);
+		revalidatePath(`/product/${body.productId}`);
+		console.log(`Revalidating /products`);
+		revalidatePath(`/products`);
+		return new Response(null, { status: 204 });
+	} else {
+		return new Response(null, { status: 400 });
+	}
 }
